32637a269bb8a3e006a444b5ed0c9e944467f419
Cyn_nyn <pkmn_rgds@yahoo.co.jp>
2018-03-10 00:36:11 +0900

    Kotlinファイルのクラス継承の準備および宣言 mod gitignore

--- SIF_ScoreCalc/Card.kt
+++ SIF_ScoreCalc/Card.kt
@@ -4,15 +4,18 @@ package sif_scorecalc
 
 import java.util.*
 
-class Card( rarity: String, skill_name: String, skill_conditions_type: String,
-			skill_conditions_size: Int, skill_effect: String, accuracy_time: Double,
+public open class Card( rarity: String, skill_name: String, skill_conditions_type: String,
+			skill_conditions_size: Int, skill_effect: String, skill_probably: Int,accuracy_time: Double,
 			card_smile: Int, card_pure: Int, card_cool: Int,
-			center_skill_name: String, subcenter_skill_name: String){
+			center_skill_name: String, subcenter_skill_name: String)
+{
+	//継承するためにpublic openに宣言を変更.
 	val rarity = rarity//カードのレアリティ
 	val skill_name = skill_name//特技名。
 	val skill_conditions_type = skill_conditions_type//スキル発動条件
 	val skill_conditions_size = skill_conditions_size//スキル発動条件数値
 	val skill_effect = skill_effect//スキルの効果 e.g.)スコア,回復,判定強化,特技発動率アップ,...etc
+	val skill_probably = skill_probably//特技発動確率.
 	val accuracy_time = accuracy_time//判定強化時間(個人所有のカードであれば、実際の数値。スキルのデータであれば特技レベル1の数値.)
 	val card_smile = card_smile//カードスマイル値
 	val card_pure = card_pure//カードピュア値
